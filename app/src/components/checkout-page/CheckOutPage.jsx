b3b27c6b15f2319b76d1f56e4ce48bd106eba6b4
Dario Arsovski <darioarsovski@Darios-MacBook-Pro.local>
2023-11-23 01:00:56 +0100

    [Authentication impl #10] Added logic for allowing only logged in users to checkout

--- app/src/components/checkout-page/CheckOutPage.jsx
+++ app/src/components/checkout-page/CheckOutPage.jsx
@@ -4,6 +4,7 @@ import CheckoutItem from "./CheckoutItem";
 
 const INITIAL_ORDER_MESSAGE = "";
 export default function CheckOutPage() {
+    const [errors, setErrors] = useState('');
     const { cart, cartSize, clearCart, clearItem } = useContext(ShoppingCartContext);
 
     const [totalPrice, setTotalPrice] = useState(0);
@@ -17,6 +18,11 @@ export default function CheckOutPage() {
     }, [cart])
 
     function cartCheckoutHandler() {
+        if (localStorage.getItem('accessToken') === null) {
+            setErrors('You must be logged in to checkout');
+            return;
+        }
+
         console.log("sending entire cart to BE");
         console.log("order preference", orderMessage)
         console.log({ cart, orderMessage });
@@ -32,6 +38,17 @@ export default function CheckOutPage() {
 
     return <main>
         <section id="cart" className="flex flex-col bg-productWhite mt-10">
+
+            {errors.length !== 0
+                && <div className=" w-1/2 self-center bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <span className="block sm:inline">{errors}</span>
+                    <span
+                        className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                    </span>
+                </div>
+            }
+
             <div id="checkout" className="flex justify-between">
                 <h1 className="text-3xl ml-4">Checkout</h1>
                 <div className="flex">
