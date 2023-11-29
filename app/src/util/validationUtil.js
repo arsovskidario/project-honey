ecd175bb89a7984b3a87288c1f50a0e601a73e1a
Dario Arsovski <darioarsovski@Darios-MacBook-Pro.local>
2023-11-29 14:19:56 +0100

    [Implement site validation #11] Added validation for register form

---
+++ app/src/util/validationUtil.js
@@ -0,0 +1,78 @@
+
+function isEmptyOrNull(str) {
+    return !str || str.trim() === '';
+}
+
+function isEmailValid(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+}
+
+function isPasswordValidFormat(password) {
+    var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[~`!@#\$%\^&\*\(\)_\+\-=\{\}\[\]\|\\:;"'<>,\./?]).{8,}$/;
+    return re.test(password);
+}
+
+function isPasswordConfirmationCorrect(password, confirmationPassword) {
+    return password == confirmationPassword;
+}
+
+function isNumeric(value) {
+    return /^-?\d+$/.test(value);
+}
+
+
+//TODO: For login if attributes not present skip them
+export const validateFields = (fields) => {
+    const errors = {};
+
+    if (!isEmailValid(fields['email'])) {
+        errors['email'] = 'Provided email is not valid!';
+    }
+
+
+    if (!isPasswordValidFormat(fields['password'])) {
+        errors['password'] = 'Please enter a password with min 8 letter, with at least a symbol[~`!@#$%^&*()-_+={}[]|\;:"<>,./?], upper and lower case letters and a number!';
+    }
+
+
+    if (!isPasswordConfirmationCorrect(fields['password'], fields['passwordConfirmation'])) {
+        errors['passwordConfirmation'] = "Passwords don't match!";
+    }
+
+
+    if (isEmptyOrNull(fields['firstName'])) {
+        errors['firstName'] = 'Please enter your first name in order to continue!';
+    }
+
+    if (isEmptyOrNull(fields['lastName'])) {
+        errors['lastName'] = 'Please enter your last name in order to continue!';
+    }
+
+
+    if (isEmptyOrNull(fields['address'])) {
+        errors['address'] = 'Please enter your address in order to continue!';
+    }
+
+    if (isEmptyOrNull(fields['city'])) {
+        errors['city'] = 'Please enter your city in order to continue!';
+    }
+
+    if(!isNumeric(fields['phoneNumber'])) {
+        errors['phoneNumber'] = 'Please enter a valid phone number!';
+    }
+
+    return errors;
+
+}
+
+export const hasErrors = (errors) => {
+    return Object.keys(errors).length !== 0
+}
+
+export const hasErrorInput = (errors, input) => {
+    return errors.hasOwnProperty(input)
+}
