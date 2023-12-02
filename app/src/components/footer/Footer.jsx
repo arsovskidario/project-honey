cdf81f9cc45a644c3356572fa47247ef11106c88
Dario Arsovski <darioarsovski@Darios-MacBook-Pro.local>
2023-12-02 18:23:47 +0100

    [About us navbar impl #17] added footer html

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -1,16 +1,29 @@
 import { Link } from 'react-router-dom';
 
-import './Footer.css'
 
 export default function Footer() {
     return (
-        <footer className="footer-container">
-            {/* Your footer content goes here */}
-            <div className="footer-content">
-                <Link to="/about">About Us</Link>
-                <Link to="/contact">Contact Us</Link>
-                {/* Add more links or content as needed */}
+
+        <footer className="bottom-0 w-full bg-white rounded-lg shadow m-4">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center">© 2023 <Link className="hover:underline">Balkan Nectar</Link>. All Rights Reserved.
+                </span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+                    <li>
+                        <Link to="" className="hover:underline me-4 md:me-6">About</Link>
+                    </li>
+                    <li>
+                        <Link to="" className="hover:underline me-4 md:me-6">Newsletter</Link>
+                    </li>
+                    <li>
+                        <Link to="" className="hover:underline me-4 md:me-6">License</Link>
+                    </li>
+                    <li>
+                        <Link to="" className="hover:underline">Contact</Link>
+                    </li>
+                </ul>
             </div>
         </footer>
+
     );
 }
