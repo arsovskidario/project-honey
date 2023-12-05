c17446d17cab2962ed39afe397293b7a2f5fcf3d
Dario Arsovski <darioarsovski@Darios-MacBook-Pro.local>
2023-12-05 22:50:25 +0100

    [About us navbar impl #17] Added validation for the update/create product modal

--- app/src/components/pages/product-details-page/details/modal/ProductDetailsModal.jsx
+++ app/src/components/pages/product-details-page/details/modal/ProductDetailsModal.jsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import useForm from "../../../../../hooks/useForm";
 import { createProduct, deleteProduct, editProductInfo } from "../../../../../services/productsService";
 import AuthContext from "../../../../../contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { validateProduct, hasErrors, hasErrorInput } from "../../../../../util/validationUtil";
 
 export default function ProductDetailsModal({
     operationType,
@@ -27,26 +29,31 @@ export default function ProductDetailsModal({
     }
 
     const submitHandler = async () => {
-        try {
-            if (operationType === 'create') {
-                const response = await createProduct(formState, accessToken);
-                navigate(`/product-details/${response._id}`);
 
-            } else if (operationType === 'update') {
-                console.log('update operatino')
-                const response = await editProductInfo(productDetails._id, formState, accessToken);
+        const validationErrors = validateProduct(formState);
+        addValidationErrors(validationErrors);
+        if (!hasErrors(validationErrors)) {
+            try {
+                if (operationType === 'create') {
+                    const response = await createProduct(formState, accessToken);
+                    navigate(`/product-details/${response._id}`);
 
-                //TDOO: should update shopping cart item here
-                updateProductDetails(response);
+                } else if (operationType === 'update') {
+                    console.log('update operatino')
+                    const response = await editProductInfo(productDetails._id, formState, accessToken);
+
+                    //TDOO: should update shopping cart item here
+                    updateProductDetails(response);
+                }
+
+            } catch (error) {
+                console.log('service unavailable');
+            } finally {
+                closeHandler();
             }
-
-        } catch (error) {
-            console.log('service unavailable');
-        } finally {
-            closeHandler();
         }
     }
-    const { formState, changeHandler, onSubmit } = useForm(productDetails, submitHandler);
+    const { formState, changeHandler, onSubmit, errors, addValidationErrors } = useForm(productDetails, submitHandler);
 
     return (
         <div
@@ -73,6 +80,12 @@ export default function ProductDetailsModal({
                                     value={formState.name}
                                     onChange={changeHandler}
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 -700 -600 -400 -primary-500 -primary-500" placeholder="Ex. Raw honey countryside" />
+
+                                {hasErrorInput(errors, 'name') &&
+                                    <div className="text-red-700 p-1 rounded relative" role="alert">
+                                        <span className="block sm:inline">{errors.name}</span>
+                                    </div>
+                                }
                             </div>
                             <div>
                                 <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 ">Category</label>
@@ -80,6 +93,11 @@ export default function ProductDetailsModal({
                                     value={formState.category}
                                     onChange={changeHandler}
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 -700 -600 -400 -primary-500 -primary-500" placeholder="Ex. honey" />
+                                {hasErrorInput(errors, 'category') &&
+                                    <div className="text-red-700 p-1 rounded relative" role="alert">
+                                        <span className="block sm:inline">{errors.category}</span>
+                                    </div>
+                                }
                             </div>
                             <div>
                                 <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 ">Price</label>
@@ -87,6 +105,11 @@ export default function ProductDetailsModal({
                                     value={formState.price}
                                     onChange={changeHandler}
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 -700 -600 -400 -primary-500 -primary-500" placeholder="$10" />
+                                {hasErrorInput(errors, 'price') &&
+                                    <div className="text-red-700 p-1 rounded relative" role="alert">
+                                        <span className="block sm:inline">{errors.price}</span>
+                                    </div>
+                                }
                             </div>
                             <div>
                                 <label htmlFor="imgUrl" className="block mb-2 text-sm font-medium text-gray-900 ">Image url</label>
@@ -94,6 +117,11 @@ export default function ProductDetailsModal({
                                     value={formState.imgUrl}
                                     onChange={changeHandler}
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 -700 -600 -400 -primary-500 -primary-500" placeholder="imgUrl" />
+                                {hasErrorInput(errors, 'imgUrl') &&
+                                    <div className="text-red-700 p-1 rounded relative" role="alert">
+                                        <span className="block sm:inline">{errors.imgUrl}</span>
+                                    </div>
+                                }
                             </div>
 
                             <div className="sm:col-span-2">
@@ -103,6 +131,11 @@ export default function ProductDetailsModal({
                                     value={formState.description}
                                     onChange={changeHandler}
                                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 -700 -600 -400 -primary-500 -primary-500" placeholder="Write a description..."></textarea>
+                                {hasErrorInput(errors, 'description') &&
+                                    <div className="text-red-700 p-1 rounded relative" role="alert">
+                                        <span className="block sm:inline">{errors.description}</span>
+                                    </div>
+                                }
                             </div>
                         </div>
                         <div className="flex items-center space-x-4">
@@ -115,7 +148,7 @@ export default function ProductDetailsModal({
                                         d="M 46.574219 3.425781 C 45.625 2.476563 44.378906 2 43.132813 2 C 41.886719 2 40.640625 2.476563 39.691406 3.425781 C 39.691406 3.425781 39.621094 3.492188 39.53125 3.585938 C 39.523438 3.59375 39.511719 3.597656 39.503906 3.605469 L 4.300781 38.804688 C 4.179688 38.929688 4.089844 39.082031 4.042969 39.253906 L 2.035156 46.742188 C 1.941406 47.085938 2.039063 47.453125 2.292969 47.707031 C 2.484375 47.898438 2.738281 48 3 48 C 3.085938 48 3.171875 47.988281 3.257813 47.964844 L 10.746094 45.957031 C 10.917969 45.910156 11.070313 45.820313 11.195313 45.695313 L 46.394531 10.5 C 46.40625 10.488281 46.410156 10.472656 46.417969 10.460938 C 46.507813 10.371094 46.570313 10.308594 46.570313 10.308594 C 48.476563 8.40625 48.476563 5.324219 46.574219 3.425781 Z M 45.160156 4.839844 C 46.277344 5.957031 46.277344 7.777344 45.160156 8.894531 C 44.828125 9.222656 44.546875 9.507813 44.304688 9.75 L 40.25 5.695313 C 40.710938 5.234375 41.105469 4.839844 41.105469 4.839844 C 41.644531 4.296875 42.367188 4 43.132813 4 C 43.898438 4 44.617188 4.300781 45.160156 4.839844 Z M 5.605469 41.152344 L 8.847656 44.394531 L 4.414063 45.585938 Z">
                                     </path>
                                 </svg>
-                                <span>Edit</span></button>
+                                <span> {operationType === 'create' ? 'Create' : 'Edit'}</span></button>
 
                             {operationType === 'create' ?
                                 (<button type="button"
