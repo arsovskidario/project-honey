60b8f3bef3309f439ea93858cbd7d1f2f4cc05ef
Dario Arsovski <darioarsovski@Darios-MacBook-Pro.local>
2023-12-07 00:55:08 +0100

    [Refinement] Added product discovery section to the main page

--- app/src/components/pages/main-page/MainPage.jsx
+++ app/src/components/pages/main-page/MainPage.jsx
@@ -2,19 +2,23 @@ import './MainPage.css'
 import Banner from '../../banner/Banner'
 import ProductList from '../../product/list/ProductList'
 import NewsLetter from './newsletter/Newsletter'
+import ProductDiscovery from './discovery/ProductDiscovery'
 
 export default function MainPage() {
     return (
         <main>
             <Banner
-             bannerUrl={'/src/assets/main-page-banner.jpg'}
-             bannerName={'Balkan Nectar'}
-             bannerDescription={'Where tradition meets the sweetness of nature.'}
-             isMainPage={true}
-             />
-            <ProductList productName ="featured products"/>
+                bannerUrl={'/src/assets/main-page-banner.jpg'}
+                bannerName={'Balkan Nectar'}
+                bannerDescription={'Where tradition meets the sweetness of nature.'}
+                isMainPage={true}
+            />
 
-            <NewsLetter/>
+            <ProductDiscovery />
+
+            <ProductList productName="featured products" />
+
+            <NewsLetter />
         </main>
     )
 }
